f219c4adf81f0e0abefdc2305caafa51746f9f52
Gerrit Hentschel <gerrit@doo.net>
2013-01-03 11:01:38 +0100

    update AWS SDK to 1.3.27

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.doo/clj-aws-s3 "0.2.2-SNAPSHOT"
   :description "Clojure Amazon S3 library."
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [com.amazonaws/aws-java-sdk "1.3.2"]
+                 [com.amazonaws/aws-java-sdk "1.3.27"]
                  [cheshire "4.0.0"]]
   :dev-dependencies [[codox "0.4.0"]])
